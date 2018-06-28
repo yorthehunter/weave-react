a4259edc037366f114510838e8e1ca7abc799ac7
yorthehunter <boru.design@gmail.com>
2018-06-28 13:41:31 -0400

    setup styleguidist to show correct components

---
+++ setupStyleguidist.js
@@ -0,0 +1,11 @@
+import PopoverDisplay from './src/components/Popover/PopoverDisplay';
+import PopoverActivator from './src/components/Popover/PopoverActivator';
+import DropdownMenu from './src/components/Dropdown/DropdownMenu';
+import DropdownActivator from './src/components/Dropdown/DropdownActivator';
+import CollectionItem from './src/components/Collection/CollectionItem';
+
+global.PopoverDisplay = PopoverDisplay;
+global.PopoverActivator = PopoverActivator;
+global.DropdownMenu = DropdownMenu;
+global.DropdownActivator = DropdownActivator;
+global.CollectionItem = CollectionItem;
--- styleguide.config.js
+++ styleguide.config.js
@@ -3,13 +3,16 @@ const path = require('path');
 module.exports = {
   styleguideDir: 'docs',
   skipComponentsWithoutExample: true,
-  // ignore: [
-  //   '**/PopoverDisplay.jsx',
-  //   '**/PopoverActivator.jsx',
-  //   '**/DropdownMenu.jsx',
-  //   '**/DropdownActivator.jsx',
-  // ],
+  ignore: [
+    '**/CollectionItem.jsx',
+    '**/PopoverDisplay.jsx',
+    '**/PopoverActivator.jsx',
+    '**/DropdownMenu.jsx',
+    '**/DropdownActivator.jsx',
+  ],
+
   require: [
+    path.resolve(__dirname, './setupStyleguidist.js'),
     path.join(__dirname, './node_modules/weave-ui/src/core/index.scss'),
   ],
   webpackConfig: {
