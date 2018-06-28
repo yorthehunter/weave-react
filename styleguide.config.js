a3b9c532cd104543c52a124e8612aa2efb088354
yorthehunter <boru.design@gmail.com>
2018-06-27 21:47:43 -0400

    Adds styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  styleguideDir: 'docs',
+  skipComponentsWithoutExample: true,
+  // ignore: [
+  //   '**/PopoverDisplay.jsx',
+  //   '**/PopoverActivator.jsx',
+  //   '**/DropdownMenu.jsx',
+  //   '**/DropdownActivator.jsx',
+  // ],
+  require: [
+    path.join(__dirname, './node_modules/weave-ui/src/core/index.scss'),
+  ],
+  webpackConfig: {
+    module: {
+      rules: [
+        // Babel loader, will use your project’s .babelrc
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query:
+            {
+              presets:['react']
+            }
+        },
+        // Other loaders that are needed for your components
+        {
+          test: /\.scss$/,
+          loaders: [
+            'style-loader',
+            'css-loader',
+            'sass-loader?precision=10',
+          ],
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2|svg)$/,
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+          },
+        },
+      ],
+    },
+  },
+};
